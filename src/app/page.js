db130aea9ed144adffe03d134c2636153237dedf
Qasim Subhani <qasimsubhani786@gmail.com>
2024-11-20 18:56:31 +0500

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,38 @@
-'use client'
 import { useEffect } from "react";
 import axios from "axios";
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Script from "next/script";
 
 export default function Home() {
-
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('/api/customers');
+      const response = await axios.get("/api/customers");
 
       //const responseData = await response.json();
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
   return (
     <>
-      <Script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossOrigin="anonymous"/>
-      <Script src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js" crossOrigin="anonymous" />
-      <Script src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js" crossOrigin="anonymous" />
-     </>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js"
+        crossOrigin="anonymous"
+      />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
+        crossOrigin="anonymous"
+      />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
+        crossOrigin="anonymous"
+      />
+    </>
   );
 }
